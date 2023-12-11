4c1fe3b1c4f86cba6436b52eb3e849889180ab4b
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-10 16:34:59 -0800

    functionality to app deleting and toggling

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 // define the variables used
-$input = $('input');
+$input = $("input");
 // console.log($input);
-$todos = $('.todos');
+$todos = $(".todos");
 // console.log($todos);
 
 // store the button in a variable
-$button = $('button');
+$button = $("button");
 // console.log($button);
 
 // Event listener for the button
-$button.on('click', (event) => {
-    // console.log('clicked')
-    const val = $input.val();
-    // console.log(val)
-    const $todo = $('<li>');
-    $todo.addClass('todo').text(val);
-    $todos.append($todo);
-    $input.val('');
-})
+$button.on("click", (event) => {
+  // console.log('clicked')
+  const val = $input.val();
+  // console.log(val)
+  const $todo = $("<li>").addClass("todo").text(val);
+  $todos.append($todo);
+  const $trash = $("<span>").addClass("trash");
+  $todo.append($trash);
+  const $edit = $("<span>").addClass("edit");
+  $todo.append($edit);
+  $input.val("");
+});
+
+// event to remove todos
+$(".todos").on("click", ".trash", function () {
+  $(this).parent(".todo").fadeOut(300);
+});
+
+// event to add checked
+$('.todos').on('click', '.todo', function(){
+    // console.log('clicked');
+    $(this).toggleClass('checked');
+});
